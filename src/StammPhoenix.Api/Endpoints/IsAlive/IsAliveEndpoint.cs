5908dcd8a51d6c52e1099f588716015f6178f520
Nico Welles <nico@welles.io>
2024-01-29 20:07:29 +0100

    Use FastEndpoints, remove MVC

---
+++ src/StammPhoenix.Api/Endpoints/IsAlive/IsAliveEndpoint.cs
@@ -0,0 +1,23 @@
+using FastEndpoints;
+using JetBrains.Annotations;
+
+namespace StammPhoenix.Api.Endpoints.IsAlive;
+
+[PublicAPI]
+public class IsAliveEndpoint : EndpointWithoutRequest<string>
+{
+    public override void Configure()
+    {
+        this.Get("/is-alive");
+        this.AllowAnonymous();
+        this.Description(d =>
+        {
+            d.Produces<string>();
+        });
+    }
+
+    public override async Task<string> ExecuteAsync(CancellationToken ct)
+    {
+        return "200 OK";
+    }
+}
--- src/StammPhoenix.Api/Program.cs
+++ src/StammPhoenix.Api/Program.cs
@@ -1,25 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+using FastEndpoints;
+using FastEndpoints.Swagger;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var builder = WebApplication.CreateBuilder();
+builder.Services
+   .AddFastEndpoints()
+   .SwaggerDocument(d =>
+   {
+      d.DocumentSettings = s =>
+      {
+         s.Title = "Stamm Phoenix API";
+         s.DocumentName = "v1";
+      };
+   });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.UseFastEndpoints()
+   .UseSwaggerGen();
 app.Run();
