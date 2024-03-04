c916353dc2f2bcf274743d29042f0c58898bfe8c
Hugo Berendi <hugo.berendi@outlook.de>
2024-03-04 14:42:13 +0100

    changed solidJs to solid

--- src/StammPhoenix.Web/astro.config.mjs
+++ src/StammPhoenix.Web/astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import solidJs from "@astrojs/solid-js";
+import solid from "@astrojs/solid-js";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    solidJs(), 
+    solid(), 
     tailwind({
       applyBaseStyles: false,
     }),
