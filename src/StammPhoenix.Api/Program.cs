043270a6dfd9aa70c5b340746f204180f269bd8b
Nico Welles <nico@welles.io>
2024-03-05 17:51:00 +0100

    Do not use top-level statements, add startup time

--- src/StammPhoenix.Api/Program.cs
+++ src/StammPhoenix.Api/Program.cs
@@ -1,28 +1,37 @@
 using FastEndpoints;
 using FastEndpoints.Swagger;
-using Microsoft.AspNetCore.Rewrite;
 
-var builder = WebApplication.CreateBuilder();
-builder.Services
-   .AddFastEndpoints()
-   .SwaggerDocument(d =>
+namespace StammPhoenix.Api;
+
+public static class Program
+{
+   public static readonly DateTime StartupTime = DateTime.UtcNow;
+
+   public static void Main(string[] args)
    {
-      d.DocumentSettings = s =>
-      {
-         s.Title = "Stamm Phoenix API";
-         s.DocumentName = "current";
-         s.Version = "Current";
-      };
-   });
+      var builder = WebApplication.CreateBuilder();
+      builder.Services
+         .AddFastEndpoints()
+         .SwaggerDocument(d =>
+         {
+            d.DocumentSettings = s =>
+            {
+               s.Title = "Stamm Phoenix API";
+               s.DocumentName = "current";
+               s.Version = "Current";
+            };
+         });
 
-var app = builder.Build();
-app.UseFastEndpoints(c =>
-   {
-      c.Endpoints.Configurator = ep =>
-      {
-         ep.DontAutoTag();
-      };
-   })
-   .UseSwaggerGen();
+      var app = builder.Build();
+      app.UseFastEndpoints(c =>
+         {
+            c.Endpoints.Configurator = ep =>
+            {
+               ep.DontAutoTag();
+            };
+         })
+         .UseSwaggerGen();
 
-app.Run();
+      app.Run();
+   }
+}
