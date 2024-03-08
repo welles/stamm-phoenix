bb859cd4b353c966fff3675546fef6363c967167
Hugo Berendi <hugo.berendi@outlook.de>
2024-03-08 07:02:38 +0100

    added node ssr adapter for astro

--- src/StammPhoenix.Web/astro.config.mjs
+++ src/StammPhoenix.Web/astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 import solid from "@astrojs/solid-js";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'hybrid',
-  integrations: [
-    solid(), 
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ]
+  output: 'hybrid',
+  integrations: [solid(), tailwind({
+    applyBaseStyles: false
+  })],
+  adapter: node({
+    mode: "standalone"
+  })
 });
