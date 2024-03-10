a6be045fe4ed625e8d15843c7d93284b0b519b36
Nico Welles <nico@welles.io>
2024-03-10 21:15:49 +0100

    Add endpoint for checking JWT token

---
+++ src/StammPhoenix.Api/Endpoints/Auth/Check/CheckEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using FastEndpoints;
+using JetBrains.Annotations;
+
+namespace StammPhoenix.Api.Endpoints.Auth.Check;
+
+[PublicAPI]
+public sealed class CheckEndpoint : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        this.Get("/check");
+        this.Roles("Leader");
+        this.Group<AuthGroup>();
+        this.Summary(s =>
+        {
+            s.Summary = "Check if the JWT token is valid";
+            s.Description = "Checks if the given bearer token is valid. Returns a 204 No Content response if validation was successful.";
+        });
+        this.Description(d =>
+        {
+            d.Produces(StatusCodes.Status204NoContent);
+            d.Produces(StatusCodes.Status401Unauthorized);
+        });
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        await this.SendNoContentAsync(ct);
+    }
+}
