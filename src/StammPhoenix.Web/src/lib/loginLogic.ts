04b0ef82f86d8b7ac82bfc327b4b25e5442ba813
Hugo Berendi <hugo.berendi@outlook.de>
2024-03-10 21:50:56 +0100

    added login logic

---
+++ src/StammPhoenix.Web/src/lib/loginLogic.ts
@@ -0,0 +1,43 @@
+// src/utils/loginLogic.ts
+
+export const login = async (email: string, password: string) => {
+  try {
+    const response = await fetch("https://dev-api.stamm-phoenix.de/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ login_email: email, password }),
+    });
+    const data = await response.json();
+    if (response.ok) {
+      return { token: data.token, error: null };
+    } else {
+      return { token: null, error: data.error };
+    }
+  } catch (error) {
+    return { token: null, error: "An error occurred while logging in." };
+  }
+};
+
+export const checkToken = async (token: string | null) => {
+  if (token) {
+    try {
+      const response = await fetch("https://dev-api.stamm-phoenix.de/auth/check", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+      if (response.ok) {
+        // Token is valid
+        console.log("Token is valid:", data);
+      } else {
+        // Token is invalid
+        console.log("Token is invalid:", data.error);
+      }
+    } catch (error) {
+      console.error("An error occurred while checking the token:", error);
+    }
+  }
+};
