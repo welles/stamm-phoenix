ea205509deae5d707a728bdbce05d24f899125a2
Hugo Berendi <hugo.berendi@outlook.de>
2024-03-20 23:10:59 +0100

    add test for login & set testing site to dev-web.stamm-phoenix.de

--- src/StammPhoenix.Web/e2e/example.spec.ts
+++ src/StammPhoenix.Web/e2e/example.spec.ts
@@ -1,18 +1,11 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test("test login", async ({ page }) => {
+  await page.goto("/");
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByLabel("E-Mail:").click();
+  await page.getByLabel("E-Mail:").fill("admin@stamm-phoenix.de");
+  await page.getByLabel("Passwort:").click();
+  await page.getByLabel("Passwort:").fill("admin");
+  await page.getByRole("button", { name: "Login" }).click();
 });
