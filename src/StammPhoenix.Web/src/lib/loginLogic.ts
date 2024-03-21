023e030bf10509ccfebe16a401a9cd6ed3a2b245
Kamachi <68025706+hugo-berendi@users.noreply.github.com>
2024-03-21 11:26:53 +0100

    Improved login code

--- src/StammPhoenix.Web/src/lib/loginLogic.ts
+++ src/StammPhoenix.Web/src/lib/loginLogic.ts
@@ -1,15 +1,13 @@
-// src/utils/loginLogic.ts
+// src/utils/authLogic.ts
 
 export interface Credentials {
-    login_email: string,
-    password: string
+  login_email: string;
+  password: string;
 }
 
-export const login = async (email: string, password: string) => {
-    try {
-
-        const creds: Credentials = { login_email: email, password };
-
+export const login = async (email: string, password: string): Promise<{ token: string | null; error: string | null }> => {
+  try {
+    const creds: Credentials = { login_email: login_email, password };
     const response = await fetch("https://dev-api.stamm-phoenix.de/auth/login", {
       method: "POST",
       headers: {
@@ -17,35 +15,38 @@ export const login = async (email: string, password: string) => {
       },
       body: JSON.stringify(creds),
     });
+
     const data = await response.json();
+
     if (response.ok) {
       return { token: data.token, error: null };
     } else {
-        return { token: null, error: data.error };
+      return { token: null, error: data.error };
     }
   } catch (error) {
-    return { token: null, error: `An error occurred while logging in. ${error}` };
+    return { token: null, error: `An error occurred while logging in: ${error}` };
   }
 };
 
-export const checkToken = async (token: string | null) => {
-  if (token) {
-    try {
-      const response = await fetch("https://dev-api.stamm-phoenix.de/auth/check", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      const data = await response.json();
-      if (response.ok) {
-        // Token is valid
-        console.log("Token is valid:", data);
-      } else {
-        // Token is invalid
-        console.log("Token is invalid:", data.error);
-      }
-    } catch (error) {
-      console.error("An error occurred while checking the token:", error);
+export const checkToken = async (token: string): Promise<boolean> => {
+  try {
+    const response = await fetch("https://dev-api.stamm-phoenix.de/auth/check", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      console.log("Token is valid:", data);
+      return true;
+    } else {
+      console.error("Token is invalid:", data.error);
+      return false;
     }
+  } catch (error) {
+    console.error("An error occurred while checking the token:", error);
+    return false;
   }
 };
