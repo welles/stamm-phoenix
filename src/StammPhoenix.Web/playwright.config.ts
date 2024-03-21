790fa3b792843d854dc81f06d10d137acb225d57
Hugo Berendi <hugo.berendi@outlook.de>
2024-03-21 06:40:12 +0100

    fixed problem with playwright timeout

--- src/StammPhoenix.Web/playwright.config.ts
+++ src/StammPhoenix.Web/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://dev-web.stamm-phoenix.de/",
+    baseURL: "http://127.0.0.1:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -67,4 +67,9 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
+  webServer: {
+    command: "bunx astro dev --host --port 3000",
+    url: "http://127.0.0.1:3000",
+    reuseExistingServer: !process.env.CI,
+  },
 });
