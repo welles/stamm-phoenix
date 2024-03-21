1f54ff7e5b743722cc10bc05e57e3b354dcc5f3f
Hugo Berendi <hugo.berendi@outlook.de>
2024-03-21 12:30:34 +0100

    use axios for http requests

--- src/StammPhoenix.Web/src/lib/loginLogic.ts
+++ src/StammPhoenix.Web/src/lib/loginLogic.ts
@@ -1,4 +1,4 @@
-// src/utils/authLogic.ts
+import axios from 'axios';
 
 export interface Credentials {
   login_email: string;
@@ -7,52 +7,45 @@ export interface Credentials {
 
 export const login = async (email: string, password: string): Promise<{ token: string | null; error: string | null }> => {
   try {
-    const creds: Credentials = { login_email: login_email, password };
-    const response = await fetch("https://dev-api.stamm-phoenix.de/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(creds),
-    });
+    const creds: Credentials = { login_email: email, password };
+    const response = await axios.post("https://dev-api.stamm-phoenix.de/auth/login", creds);
 
-    const data = await response.json();
-
-    if (response.ok) {
-      return { token: data.token, error: null };
+    if (response.status === 200) {
+      return { token: response.data.token, error: null };
     } else {
-      return { token: null, error: data.error };
+      return { token: null, error: response.data.error };
     }
   } catch (error) {
-    return { token: null, error: `An error occurred while logging in: ${error}` };
+    if (axios.isAxiosError(error)) {
+      return { token: null, error: `An error occurred while logging in: ${error.message}` };
+    } else {
+      return { token: null, error: `An error occurred while logging in: ${error}` };
+    }
   }
 };
 
 export const checkToken = async (token: string): Promise<boolean> => {
   try {
-    const response = await fetch("https://dev-api.stamm-phoenix.de/auth/check", {
+    const response = await axios.get("https://dev-api.stamm-phoenix.de/auth/check", {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
 
-    // Check if the response is valid JSON
-    if (!response.ok) {
-      console.error("Token is invalid:", response.statusText);
-      return false;
-    }
-
-    const data = await response.json();
-
-    if (response.ok) {
-      console.log("Token is valid:", data);
+    if (response.status === 200) {
+      console.log("Token is valid:", response.data);
       return true;
     } else {
-      console.error("Token is invalid:", data.error);
+      console.error("Token is invalid:", response.data.error);
       return false;
     }
   } catch (error) {
-    console.error("An error occurred while checking the token:", error);
+    if (axios.isAxiosError(error)) {
+      console.error("An error occurred while checking the token:", error.message);
+    } else {
+      console.error("An error occurred while checking the token:", error);
+    }
     return false;
   }
 };
+
