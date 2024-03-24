cd1765511a6a12ab8069a007b3e11c8b31f5540e
Hugo Berendi <hugo.berendi@outlook.de>
2024-03-24 12:26:43 +0100

    change default lang to de and add impressum

--- src/StammPhoenix.Web/astro-i18next.config.mts
+++ src/StammPhoenix.Web/astro-i18next.config.mts
@@ -1,5 +1,5 @@
-/** @type {import('astro-i18next').AstroI18nextConfig} */ 
+/** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
-    defaultLocale: 'en',
-    locales: ['en', 'de'],
-};
+	defaultLocale: 'de',
+	locales: ['en', 'de'],
+}
--- src/StammPhoenix.Web/astro.config.mjs
+++ src/StammPhoenix.Web/astro.config.mjs
@@ -1,25 +1,25 @@
-import { defineConfig } from 'astro/config';
-import solid from "@astrojs/solid-js";
-import tailwind from "@astrojs/tailwind";
-import astroI18next from 'astro-i18next';
-import node from '@astrojs/node';
+import { defineConfig } from 'astro/config'
+import solid from '@astrojs/solid-js'
+import tailwind from '@astrojs/tailwind'
+import astroI18next from 'astro-i18next'
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
-  	output: 'server',
-  	integrations: [
-        solid(), 
-        tailwind({
-    		applyBaseStyles: false
-  	    }),
-        astroI18next(),
-    ],
-  	adapter: node({ mode: "standalone" }),
-    i18n: {
-        defaultLocale: "en",
-        locales: ["en", "de"],
-        routing: {
-            prefixDefaultLocale: false
-        }
-    }
-});
+	output: 'server',
+	integrations: [
+		solid(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		astroI18next(),
+	],
+	adapter: node({ mode: 'standalone' }),
+	i18n: {
+		defaultLocale: 'de',
+		locales: ['en', 'de'],
+		routing: {
+			prefixDefaultLocale: false,
+		},
+	},
+})
