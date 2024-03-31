2c50fc88a519f2587b2159bb36d40fa9b200cfe3
Hugo Berendi <hugo.berendi@outlook.de>
2024-03-31 21:54:21 +0200

    change import to type import

--- src/StammPhoenix.Web/src/lib/loginLogic.ts
+++ src/StammPhoenix.Web/src/lib/loginLogic.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError, type AxiosResponse } from 'axios'
+import axios, { type AxiosError, type AxiosResponse } from 'axios'
 
 export interface Credentials {
 	login_email: string
