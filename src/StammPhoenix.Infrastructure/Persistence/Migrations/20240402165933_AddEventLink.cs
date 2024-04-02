aea895f11efdebddbf3ba3ee36103af645c31c25
Nico Welles <nico@welles.io>
2024-04-02 19:00:26 +0200

    Add event link migration

---
+++ src/StammPhoenix.Infrastructure/Persistence/Migrations/20240402165933_AddEventLink.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StammPhoenix.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEventLink : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "link",
+                table: "events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "link",
+                table: "events");
+        }
+    }
+}
