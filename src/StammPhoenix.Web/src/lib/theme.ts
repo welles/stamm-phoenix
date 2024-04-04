a6bb5a373212d1a9d62acbff34958b403845007c
Hugo Berendi <hugo.berendi@outlook.de>
2024-04-05 00:32:28 +0200

    refactor(web): refactored files to use the new API requests & formatted a few files
    Use new API requests from src/lib/api in all files
    Formatted all files with new prettier and eslint rules

--- src/StammPhoenix.Web/src/lib/theme.ts
+++ src/StammPhoenix.Web/src/lib/theme.ts
@@ -1,41 +1,41 @@
 // src/utils/theme.ts
 interface ThemeModule {
-  toggleTheme: () => void;
-  initTheme: () => void;
+	toggleTheme: () => void
+	initTheme: () => void
 }
 
 const theme: ThemeModule = (() => {
-  function toggleTheme() {
-    const body = document.body;
-    const isDarkMode = body.classList.contains('dark');
+	function toggleTheme() {
+		const body = document.body
+		const isDarkMode = body.classList.contains('dark')
 
-    if (isDarkMode) {
-      body.classList.remove('dark');
-      if (typeof window !== 'undefined') {
-        localStorage.setItem('theme', 'light');
-      }
-    } else {
-      body.classList.add('dark');
-      if (typeof window !== 'undefined') {
-        localStorage.setItem('theme', 'dark');
-      }
-    }
-  }
+		if (isDarkMode) {
+			body.classList.remove('dark')
+			if (typeof window !== 'undefined') {
+				localStorage.setItem('theme', 'light')
+			}
+		} else {
+			body.classList.add('dark')
+			if (typeof window !== 'undefined') {
+				localStorage.setItem('theme', 'dark')
+			}
+		}
+	}
 
-  function initTheme() {
-    if (typeof window !== 'undefined') {
-      const storedTheme = localStorage.getItem('theme');
-      const body = document.body;
+	function initTheme() {
+		if (typeof window !== 'undefined') {
+			const storedTheme = localStorage.getItem('theme')
+			const body = document.body
 
-      if (storedTheme === 'dark') {
-        body.classList.add('dark');
-      } else {
-        body.classList.remove('dark');
-      }
-    }
-  }
+			if (storedTheme === 'dark') {
+				body.classList.add('dark')
+			} else {
+				body.classList.remove('dark')
+			}
+		}
+	}
 
-  return { toggleTheme, initTheme };
-})();
+	return { toggleTheme, initTheme }
+})()
 
-export default theme;
+export default theme
