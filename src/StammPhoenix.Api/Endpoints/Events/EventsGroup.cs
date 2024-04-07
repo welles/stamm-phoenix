d4180858cbd73671754bc33220227d90f3c6672a
Nico Welles <nico@welles.io>
2024-04-07 14:09:53 +0200

    Add endpoint group for events

---
+++ src/StammPhoenix.Api/Endpoints/Events/EventsGroup.cs
@@ -0,0 +1,7 @@
+﻿using JetBrains.Annotations;
+using StammPhoenix.Api.Core;
+
+namespace StammPhoenix.Api.Endpoints.Events;
+
+[PublicAPI]
+public class EventsGroup() : EndpointGroup("Events", "/events", "Endpoints for handling events");
