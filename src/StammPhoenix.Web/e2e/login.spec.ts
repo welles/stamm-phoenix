9a569aabf88a07137b47c2fc1455c9e60150e55e
hugo-berendi <hugo-berendi@outlook.de>
2024-07-07 14:01:27 +0200

    fix(web): fixed wrong or missing imports

--- src/StammPhoenix.Web/e2e/login.spec.ts
+++ src/StammPhoenix.Web/e2e/login.spec.ts
@@ -1,29 +1,30 @@
-import { test } from "@playwright/test"
+import { test } from "@playwright/test";
 
 test("Login Test", async ({ page }) => {
-	// Navigate to the website
-	await page.goto("/")
-const vp_width: int | undefined = page.viewportSize?.width
-	if (!(vp_width <= 550)) {
-		// Click on the Login link in the navbar
-	await page.getByRole("link", { name: "Anmelden" }).click()
+  // Navigate to the website
+  await page.goto("/");
+  const viewportSize = page.viewportSize();
+  const vp_width: number | undefined = viewportSize?.width;
+  if (vp_width !== undefined && vp_width > 550) {
+    // Click on the Login link in the navbar
+    await page.getByRole("link", { name: "Anmelden" }).click();
 
-	// Fill in the email and password fields
-	await page.getByLabel("E-Mail:").click()
-	await page.getByLabel("E-Mail:").fill("admin@stamm-phoenix.de")
-	await page.getByLabel("Passwort:").click()
-	await page.getByLabel("Passwort:").fill("admin")
+    // Fill in the email and password fields
+    await page.getByLabel("E-Mail:").click();
+    await page.getByLabel("E-Mail:").fill("admin@stamm-phoenix.de");
+    await page.getByLabel("Passwort:").click();
+    await page.getByLabel("Passwort:").fill("admin");
 
-	// Click the Login button
-	await page.getByRole("button", { name: "Anmelden" }).click()
+    // Click the Login button
+    await page.getByRole("button", { name: "Anmelden" }).click();
 
-	// Check if there is a cookie named jwt or if the URL is /success
-	const cookies = await page.context().cookies()
-	const jwtCookie = cookies.find((cookie) => cookie.name === "jwt")
+    // Check if there is a cookie named jwt or if the URL is /success
+    const cookies = await page.context().cookies();
+    const jwtCookie = cookies.find((cookie) => cookie.name === "jwt");
 
-	if (jwtCookie) {
-	} else {
-		throw new Error("Login failed, no JWT cookie")
-	}
-	}
-})
+    if (jwtCookie) {
+    } else {
+      throw new Error("Login failed, no JWT cookie");
+    }
+  }
+});
