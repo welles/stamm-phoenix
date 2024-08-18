58c42cc9e80ef55f933e2430821f6cb68b983a12
hugo-berendi <hugo.berendi@outlook.de>
2024-08-19 01:09:27 +0200

    refactor(web)!: use sveltekit
    Using sveltekit because it's is more performant and I want to try new things >:3

---
+++ src/StammPhoenix.Web/svelte.config.js
@@ -0,0 +1,17 @@
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "svelte-adapter-bun";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			envPrefix: "STAMM_PHOENIX_",
+		}),
+	},
+};
+
+export default config;
